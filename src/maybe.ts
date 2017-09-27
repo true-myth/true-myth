3e7f76eb537e26f157981d4d153896aae6c78cad
Chris Krycho <chris@chriskrycho.com>
2017-09-27 17:19:35 -0400

    Finish `Maybe` in class style, with tests.

--- src/maybe.ts
+++ src/maybe.ts
@@ -1,60 +1,144 @@
+const isCthulhu = (value: any): value is undefined | null =>
+  typeof value === 'undefined' || value === null;
+
 export enum Variant {
   Some = 'Some',
   Nothing = 'Nothing',
 }
 
-const isCthulhu = (value: any): value is undefined | null =>
-  typeof value === 'undefined' || value === null;
+export type MapFn<T, U> = (t: T) => U;
 
-export type Some<T> = {
-  variant: Variant.Some;
-  value: T;
-};
+// Someday maybe we'll have `protocol`s and this would just have default
+// implementations for nearly everything in the concrete classes below.
+export interface IMaybe<T> {
+  variant: Variant;
+  map<U>(this: Maybe<T>, mapFn: MapFn<T, U>): Maybe<U>;
+  mapOr<U>(this: Maybe<T>, orU: U, mapFn: MapFn<T, U>): U;
+  mapOrElse<U>(this: Maybe<T>, orElseFn: (...args: any[]) => U, mapFn: MapFn<T, U>): U;
+  or(this: Maybe<T>, mOr: Maybe<T>): Maybe<T>;
+  orElse(this: Maybe<T>, orElseFn: (...args: any[]) => Maybe<T>): Maybe<T>;
+  and<U>(this: Maybe<T>, mAnd: Maybe<U>): Maybe<U>;
+  andThen<U>(this: Maybe<T>, andThenFn: (t: T) => Maybe<U>): Maybe<U>;
+  unwrap(): T | never;
+  unwrapOrElse(this: Maybe<T>, elseFn: (...args: any[]) => T): T;
+}
 
-export const Some = <T>(value: T | null | undefined): Maybe<T> => {
-  if (isCthulhu(value)) {
-    throw new Error(`called Some() with ${value === null ? 'null' : typeof value}`);
+export class Some<T> implements IMaybe<T> {
+  private value: T;
+
+  variant = Variant.Some;
+
+  constructor(value: T | null | undefined) {
+    if (isCthulhu(value)) {
+      throw new Error('Tried to construct `Some` with `null` or `undefined`');
+    }
+
+    this.value = value;
   }
 
-  return {
-    variant: Variant.Some,
-    value: value,
-  };
-};
+  map<U>(this: Maybe<T>, mapFn: MapFn<T, U>): Maybe<U> {
+    return map(mapFn, this);
+  }
 
-export type Nothing = { variant: Variant.Nothing };
+  mapOr<U>(this: Maybe<T>, orU: U, mapFn: MapFn<T, U>): U {
+    return mapOr(orU, mapFn, this);
+  }
 
-const __Nothing: Nothing = Object.freeze({ variant: Variant.Nothing as Variant.Nothing });
+  mapOrElse<U>(this: Maybe<T>, orElseFn: (...args: any[]) => U, mapFn: MapFn<T, U>): U {
+    return mapOrElse(orElseFn, mapFn, this);
+  }
 
-export const Nothing = <T>(): Maybe<T> => __Nothing;
+  or(this: Maybe<T>, mOr: Maybe<T>): Maybe<T> {
+    return or(mOr, this);
+  }
 
-export type Maybe<T> = Some<T> | Nothing;
-export const Maybe = <T>(value: T | null | undefined): Maybe<T> =>
-  isCthulhu(value) ? Nothing() as Maybe<T> : Some(value);
+  orElse(this: Maybe<T>, orElseFn: (...args: any[]) => Maybe<T>): Maybe<T> {
+    return orElse(orElseFn, this);
+  }
 
-export const of = Maybe;
+  and<U>(this: Maybe<T>, mAnd: Maybe<U>): Maybe<U> {
+    return and(mAnd, this);
+  }
+
+  andThen<U>(this: Maybe<T>, andThenFn: (t: T) => Maybe<U>): Maybe<U> {
+    return andThen(andThenFn, this);
+  }
+
+  unwrap(): T {
+    return this.value;
+  }
+
+  unwrapOrElse(this: Maybe<T>, elseFn: (...args: any[]) => T): T {
+    return unwrapOrElse(elseFn, this);
+  }
+}
+
+export class Nothing<T> implements IMaybe<T> {
+  variant = Variant.Nothing;
+
+  map<U>(this: Maybe<T>, mapFn: MapFn<T, U>): Maybe<U> {
+    return map(mapFn, this);
+  }
+
+  mapOr<U>(this: Maybe<T>, orU: U, mapFn: MapFn<T, U>): U {
+    return mapOr(orU, mapFn, this);
+  }
+
+  mapOrElse<U>(this: Maybe<T>, orElseFn: (...args: any[]) => U, mapFn: MapFn<T, U>): U {
+    return mapOrElse(orElseFn, mapFn, this);
+  }
+
+  or(this: Maybe<T>, mOr: Maybe<T>): Maybe<T> {
+    return or(mOr, this);
+  }
+
+  orElse(this: Maybe<T>, orElseFn: (...args: any[]) => Maybe<T>): Maybe<T> {
+    return orElse(orElseFn, this);
+  }
+
+  and<U>(this: Maybe<T>, mAnd: Maybe<U>): Maybe<U> {
+    return and(mAnd, this);
+  }
+
+  andThen<U>(this: Maybe<T>, andThenFn: (t: T) => Maybe<U>): Maybe<U> {
+    return andThen(andThenFn, this);
+  }
+
+  unwrap(): never {
+    throw new Error('Tried to `unwrap(Nothing)`');
+  }
+
+  unwrapOrElse(this: Maybe<T>, elseFn: (...args: any[]) => T): T {
+    return unwrapOrElse(elseFn, this);
+  }
+}
 
 export const isSome = <T>(m: Maybe<T>): m is Some<T> => m.variant === Variant.Some;
-export const isNothing = <T>(m: Maybe<T>): m is Nothing => m.variant === Variant.Nothing;
+export const isNothing = <T>(m: Maybe<T>): m is Nothing<T> => m.variant === Variant.Nothing;
 
-type Mapper<T, U> = (t: T) => U;
+export const some = <T>(value: T) => new Some<T>(value);
+export const nothing = <T>() => new Nothing<T>();
 
-export const map = <T, U>(mapFn: Mapper<T, U>, mt: Maybe<T>): Maybe<U> =>
-  isSome(mt) ? Some(mapFn(mt.value)) : mt;
+export type Maybe<T> = Some<T> | Nothing<T>;
+export const of = <T>(value: T | undefined | null): Maybe<T> =>
+  isCthulhu(value) ? nothing<T>() : some(value);
 
-export const mapOr = <T, U>(mapFn: Mapper<T, U>, u: U, mt: Maybe<T>): Maybe<U> =>
-  isSome(mt) ? Some(mapFn(mt.value)) : Some(u);
+export const map = <T, U>(mapFn: MapFn<T, U>, mt: Maybe<T>): Maybe<U> =>
+  isSome(mt) ? some(mapFn(unwrap(mt))) : nothing<U>();
+
+export const mapOr = <T, U>(orU: U, mapFn: MapFn<T, U>, mt: Maybe<T>): U =>
+  isSome(mt) ? mapFn(unwrap(mt)) : orU;
 
 export const mapOrElse = <T, U>(
-  mapFn: Mapper<T, U>,
-  elseFn: (...args: any[]) => U,
+  orElseFn: (...args: any[]) => U,
+  mapFn: MapFn<T, U>,
   mt: Maybe<T>
-): U => (isSome(mt) ? mapFn(mt.value) : elseFn());
+): U => (isSome(mt) ? mapFn(unwrap(mt)) : orElseFn());
 
-export const and = <T, U>(mu: Maybe<U>, mt: Maybe<T>): Maybe<U> => (isSome(mt) ? mu : Nothing());
+export const and = <T, U>(mu: Maybe<U>, mt: Maybe<T>): Maybe<U> => (isSome(mt) ? mu : nothing());
 
 export const andThen = <T, U>(thenFn: (t: T) => Maybe<U>, mt: Maybe<T>): Maybe<U> =>
-  isSome(mt) ? thenFn(mt.value) : Nothing();
+  isSome(mt) ? thenFn(unwrap(mt)) : nothing();
 
 export const or = <T>(mDef: Maybe<T>, mt: Maybe<T>): Maybe<T> => (isSome(mt) ? mt : mDef);
 
@@ -62,14 +146,8 @@ export const orElse = <T>(elseFn: (...args: any[]) => Maybe<T>, mt: Maybe<T>): M
   isSome(mt) ? mt : elseFn();
 
 export const unwrap = <T>(mt: Maybe<T>): T => {
-  if (isSome(mt)) {
-    return mt.value;
-  }
-
-  throw new Error('Tried to `get(Nothing)`');
+  return mt.unwrap();
 };
 
-export const unwrapOrElse = <T>(mt: Maybe<T>, f: (...args: any[]) => T): T =>
-  isSome(mt) ? mt.value : f();
-
-export default Maybe;
+export const unwrapOrElse = <T>(orElseFn: (...args: any[]) => T, mt: Maybe<T>): T =>
+  isSome(mt) ? unwrap(mt) : orElseFn();
--- test/maybe.test.ts
+++ test/maybe.test.ts
@@ -3,28 +3,30 @@ import * as Maybe from '../src/maybe';
 
 type Neat = { neat: string };
 
-test('`Maybe.Some`', () => {
-  const shouldBeFine = Maybe.Maybe({ neat: 'string' });
+const length = (s: string) => s.length;
+
+test('`Maybe.some`', () => {
+  const shouldBeFine = Maybe.of({ neat: 'string' });
   assertType<Maybe.Maybe<Neat>>(shouldBeFine);
-  const shouldBeNeat = Maybe.Maybe<Neat>(undefined);
+  const shouldBeNeat = Maybe.of<Neat>(undefined);
   assertType<Maybe.Maybe<Neat>>(shouldBeNeat);
 
-  const someString: Maybe.Maybe<string> = Maybe.Some('string');
+  const someString: Maybe.Maybe<string> = Maybe.of('string');
   switch (someString.variant) {
     case Maybe.Variant.Some:
-      expect(someString.value).toBe('string');
+      expect(someString.unwrap()).toBe('string');
       break;
     case Maybe.Variant.Nothing:
       expect(false).toBe(true); // because this should never happen
       break;
   }
 
-  expect(() => Maybe.Some(null)).toThrow();
-  expect(() => Maybe.Some(undefined)).toThrow();
+  expect(() => Maybe.some(null)).toThrow();
+  expect(() => Maybe.some(undefined)).toThrow();
 });
 
-test('`Maybe.Nothing`', () => {
-  const nothing: Maybe.Maybe<number> = Maybe.Nothing();
+test('`Maybe.nothing`', () => {
+  const nothing = Maybe.nothing();
   switch (nothing.variant) {
     case Maybe.Variant.Some:
       expect(true).toBe(false); // because this should never happen
@@ -34,7 +36,7 @@ test('`Maybe.Nothing`', () => {
       break;
   }
 
-  const nothingOnType = Maybe.Nothing<string>();
+  const nothingOnType = Maybe.nothing<string>();
   assertType<Maybe.Maybe<string>>(nothingOnType);
 });
 
@@ -68,45 +70,43 @@ test('`Maybe.of` with values', () => {
 });
 
 test('`Maybe.map`', () => {
-  const length = (s: string) => s.length;
-
   const someString = Maybe.of('string');
   const itsLength = Maybe.map(length, someString);
   assertType<Maybe.Maybe<number>>(itsLength);
-  expect(itsLength).toEqual(Maybe.Some('string'.length));
+  expect(itsLength).toEqual(Maybe.some('string'.length));
 
   const none = Maybe.of<string>(null);
   const noLength = Maybe.map(length, none);
   assertType<Maybe.Maybe<string>>(none);
-  expect(noLength).toEqual(Maybe.Nothing());
+  expect(noLength).toEqual(Maybe.nothing());
 });
 
 test('`Maybe.mapOr`', () => {
-  expect(Maybe.mapOr(x => x.length, 0, Maybe.Some('string'))).toEqual(Maybe.Some('string'.length));
-  expect(Maybe.mapOr(x => x.length, 0, Maybe.of<string>(null))).toEqual(Maybe.Some(0));
+  expect(Maybe.mapOr(0, x => x.length, Maybe.some('string'))).toEqual('string'.length);
+  expect(Maybe.mapOr(0, x => x.length, Maybe.of<string>(null))).toEqual(0);
 });
 
 test('`Maybe.mapOrElse`', () => {
   const length = (s: { length: number }) => s.length;
   const string = 'a string';
-  expect(Maybe.mapOrElse(length, () => 0, Maybe.of('a string'))).toBe(string.length);
-  expect(Maybe.mapOrElse(length, () => 0, Maybe.Nothing())).toBe(0);
+  expect(Maybe.mapOrElse(() => 0, length, Maybe.of('a string'))).toBe(string.length);
+  expect(Maybe.mapOrElse(() => 0, length, Maybe.nothing())).toBe(0);
 });
 
 test('`Maybe.and`', () => {
-  expect(Maybe.and(Maybe.of(42), Maybe.of('string'))).toEqual(Maybe.Some(42));
-  expect(Maybe.and(Maybe.Nothing(), Maybe.of('string'))).toEqual(Maybe.Nothing());
-  expect(Maybe.and(Maybe.Nothing(), Maybe.of(42))).toEqual(Maybe.Nothing());
-  expect(Maybe.and(Maybe.Nothing(), Maybe.Nothing())).toEqual(Maybe.Nothing());
+  expect(Maybe.and(Maybe.of(42), Maybe.of('string'))).toEqual(Maybe.some(42));
+  expect(Maybe.and(Maybe.nothing(), Maybe.of('string'))).toEqual(Maybe.nothing());
+  expect(Maybe.and(Maybe.nothing(), Maybe.of(42))).toEqual(Maybe.nothing());
+  expect(Maybe.and(Maybe.nothing(), Maybe.nothing())).toEqual(Maybe.nothing());
 });
 
 test('`Maybe.andThen`', () => {
   const strNum = Maybe.of('42');
   const number = Maybe.of(42);
   const toNumber = (x: string) => Maybe.of(Number(x));
-  const toNothing = (x: string) => Maybe.Nothing<number>();
-  const noString = Maybe.Nothing<string>();
-  const noNumber = Maybe.Nothing<number>();
+  const toNothing = (x: string) => Maybe.nothing<number>();
+  const noString = Maybe.nothing<string>();
+  const noNumber = Maybe.nothing<number>();
 
   expect(Maybe.andThen(toNumber, strNum)).toEqual(number);
   expect(Maybe.andThen(toNothing, strNum)).toEqual(noNumber);
@@ -117,7 +117,7 @@ test('`Maybe.andThen`', () => {
 test('`Maybe.or`', () => {
   const someAnswer = Maybe.of('42');
   const someWaffles = Maybe.of('waffles');
-  const nothing = Maybe.Nothing();
+  const nothing = Maybe.nothing();
 
   expect(Maybe.or(someAnswer, someWaffles)).toBe(someWaffles);
   expect(Maybe.or(nothing, someWaffles)).toBe(someWaffles);
@@ -126,18 +126,133 @@ test('`Maybe.or`', () => {
 });
 
 test('`Maybe.orElse`', () => {
-  expect(Maybe.orElse(() => Maybe.of('waffles'), Maybe.of('42'))).toEqual(Maybe.Some('42'));
-  expect(Maybe.orElse(() => Maybe.of('waffles'), Maybe.of(null))).toEqual(Maybe.Some('waffles'));
-  expect(Maybe.orElse(() => Maybe.of(null), Maybe.of('42'))).toEqual(Maybe.Some('42'));
-  expect(Maybe.orElse(() => Maybe.of(null), Maybe.of(null))).toEqual(Maybe.Nothing());
+  expect(Maybe.orElse(() => Maybe.of('waffles'), Maybe.of('42'))).toEqual(Maybe.some('42'));
+  expect(Maybe.orElse(() => Maybe.of('waffles'), Maybe.of(null))).toEqual(Maybe.some('waffles'));
+  expect(Maybe.orElse(() => Maybe.of(null), Maybe.of('42'))).toEqual(Maybe.some('42'));
+  expect(Maybe.orElse(() => Maybe.of(null), Maybe.of(null))).toEqual(Maybe.nothing());
 });
 
 test('`Maybe.unwrap`', () => {
   expect(Maybe.unwrap(Maybe.of('42'))).toBe('42');
-  expect(() => Maybe.unwrap(Maybe.Nothing())).toThrow();
+  expect(() => Maybe.unwrap(Maybe.nothing())).toThrow();
 });
 
 test('`Maybe.unwrapOrElse`', () => {
-  expect(Maybe.unwrapOrElse(Maybe.of(42), () => 100)).toBe(42);
-  expect(Maybe.unwrapOrElse(Maybe.Nothing(), () => 42)).toBe(42);
+  expect(Maybe.unwrapOrElse(() => 100, Maybe.of(42))).toBe(42);
+  expect(Maybe.unwrapOrElse(() => 42, Maybe.nothing())).toBe(42);
+});
+
+test('`Some.map`', () => {
+  expect(Maybe.some(12).map(x => x + 2)).toEqual(Maybe.some(14));
+});
+
+test('`Some.mapOr`', () => {
+  expect(Maybe.some(42).mapOr(1, x => x * 2)).toEqual(84);
+});
+
+test('`Some.mapOrElse`', () => {
+  const someString = Maybe.some('string');
+  const aDefault = () => 0;
+
+  expect(someString.mapOrElse(aDefault, length)).toEqual(6);
+});
+
+test('`Some.or`', () => {
+  const someThing = Maybe.some({ neat: 'thing' });
+  const someWaffles = Maybe.some({ neat: 'waffles' });
+
+  expect(someThing.or(someWaffles)).toEqual(someThing);
+  expect(someThing.or(Maybe.nothing())).toEqual(someThing);
+});
+
+test('`Some.orElse`', () => {
+  const someThing = Maybe.some(12);
+  const getAnswer = () => Maybe.some(42);
+
+  expect(someThing.orElse(getAnswer)).toEqual(someThing);
+});
+
+test('`Some.and`', () => {
+  const someThing = Maybe.some({ neat: 'thing' });
+  const somethingElse = Maybe.some(['amazing', { tuple: 'thing' }]);
+
+  expect(someThing.and(somethingElse)).toEqual(somethingElse);
+});
+
+test('`Some.andThen`', () => {
+  const someThing = Maybe.some({ Jedi: 'Luke Skywalker' });
+  const toDescription = (dict: { [key: string]: string }) =>
+    Maybe.some(
+      Object.keys(dict)
+        .map(key => `${dict[key]} is a ${key}`)
+        .join('\n')
+    );
+
+  expect(someThing.andThen(toDescription)).toEqual(Maybe.some('Luke Skywalker is a Jedi'));
+});
+
+test('`Some.unwrap`', () => {
+  const value = 'value';
+  const something = Maybe.some(value);
+  expect(something.unwrap()).toEqual(value);
+  expect(() => something.unwrap()).not.toThrow();
+});
+
+test('`Some.unwrapOrElse`', () => {
+  const value = 'value';
+  const something = Maybe.some(value);
+  expect(something.unwrapOrElse(() => 'other value')).toEqual(value);
+});
+
+test('`Nothing.map`', () => {
+  const nada = Maybe.nothing<string>();
+  expect(nada.map(x => x.length)).toEqual(nada);
+});
+
+test('`Nothing.mapOr`', () => {
+  const zip = Maybe.nothing<number>();
+  expect(zip.mapOr('yay', String)).toEqual('yay');
+});
+
+test('`Nothing.mapOrElse`', () => {
+  const zilch = Maybe.nothing<Neat>();
+  expect(zilch.mapOrElse(() => 'potatoes', x => x.neat)).toEqual('potatoes');
+});
+
+test('`Nothing.or`', () => {
+  const whiff = Maybe.nothing<boolean>(); // the worst: optional booleans!
+  const alt = Maybe.some(false);
+
+  expect(whiff.or(alt)).toEqual(alt);
+});
+
+test('`Nothing.orElse`', () => {
+  const notABloomingThing = Maybe.nothing<{ here: string[] }>();
+  const fallbackValue = Maybe.some({ here: ['to', 'see'] });
+  const fallback = () => fallbackValue;
+
+  expect(notABloomingThing.orElse(fallback)).toEqual(fallbackValue);
+});
+
+test('`Nothing.and`', () => {
+  const zeroStuff = Maybe.nothing<Array<string>>();
+  expect(zeroStuff.and(Maybe.some('blaster bolts'))).toEqual(zeroStuff);
+});
+
+test('`Nothing.andThen`', () => {
+  const zanilchzors = Maybe.nothing();
+  const alt = Maybe.some('string');
+
+  expect(zanilchzors.andThen(() => alt)).toEqual(zanilchzors);
+});
+
+test('`Nothing.unwrap`', () => {
+  const noStuffAtAll = Maybe.of(null);
+  expect(() => noStuffAtAll.unwrap()).toThrow();
+});
+
+test('`Nothing.unwrapOrElse`', () => {
+  const srslyNothingHereYo = Maybe.of(undefined);
+  const sokay = 'it be all fine tho';
+  expect(srslyNothingHereYo.unwrapOrElse(() => sokay)).toEqual(sokay);
 });
