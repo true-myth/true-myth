e9b46ecdb1f710846b13ca6d8754c368b3ca3ddf
Chris Krycho <chris@chriskrycho.com>
2017-09-27 22:23:48 -0400

    More documentation in earnest on `Maybe`.

--- README.md
+++ README.md
@@ -3,6 +3,44 @@
 This is a library for safe functional programming in JavaScript, with
 first-class support for TypeScript and Flow.
 
+## What is this for?
+
+- [ ] TODO: motivation
+
+### What's with the name?
+
+- [ ] TODO: Tolkien/Lewis, also Folktale/FantasyLand/Sanctuary. Nice resonance
+
+## Setup
+
+### JavaScript
+
+- [ ] TODO: JS setup
+
+### TypeScript
+
+- [ ] TODO: TS setup
+
+### Flow
+
+- [ ] TODO: Flow setup
+
+## Roadmap
+
+Before this hits 1.0, I will do:
+
+- [ ] `Maybe`
+    - [ ] add aliases for the standard names, e.g. `bind`, `chain`, etc.
+    - [ ] finish documentation
+
+- [ ] `Result`
+    - [ ] implement
+    - [ ] document
+
+If you think another type should be in this list, please [open an issue]!
+
+[open an issue]: https://github.com/chriskrycho/true-myth/issues
+
 ## Why not...
 
 There are other great functional programming libraries out there... so why not
@@ -16,8 +54,8 @@ However, there are two main reasons you might prefer <this library> to Folktale:
 1.  This is TypeScript-first and Flow-first, which means that it assumes you are
     using one of those tools if you're aiming for rigorous type safety. Folktale
     is a JavaScript-first library, with runtime checking built in for its types;
-    TypeScript support is in-work but will be quite secondary until a TypeScript
-    rewrite of the whole Folktale library lands... eventually.
+    TypeScript support is in-progress but will be quite secondary until a
+    TypeScript rewrite of the whole Folktale library lands... eventually.
    
     There's value in both of these approaches, so this library aims to take
     advantage of the compilers and play in a no-runtime-cost space instead.
@@ -37,6 +75,8 @@ However, there are two main reasons you might prefer <this library> to Folktale:
     the standard FP names for them, this library aims at people just coming up
     to speed on these ideas.
 
+There were also a few nitpicky things about Folktale's API that weren't to
+
 ### Sanctuary?
 
-[TODO: kind of the same, but specific details]
+- [ ] TODO: kind of the same as Folktale, but add specific details
--- src/maybe.ts
+++ src/maybe.ts
@@ -1,4 +1,16 @@
 /**
+ * # `Maybe`
+ * 
+ * A `Maybe<T>` is a value of type `T` which may or may not be present.
+ * 
+ * If the value is present, it is `Some(value)`. If it's absent, it's `Nothing`.
+ * This provides a type-safe container for dealing with the possibility that
+ * there's nothing here – a container you can do many of the same things you
+ * might with an array – so that you can avoid nasty `null` and `undefined`
+ * checks throughout your codebase.
+ */
+
+/**
  * Check if the value here is an all-consuming monstrosity which will consume
  * everything in its transdimensional rage. A.k.a. `null` or `undefined`.
  */
@@ -10,15 +22,17 @@ export enum Variant {
   Nothing = 'Nothing',
 }
 
-export type MapFn<T, U> = (t: T) => U;
-
 // Someday maybe we'll have `protocol`s and this would just have default
 // implementations for nearly everything in the concrete classes below.
 export interface IMaybe<T> {
+  /**
+   * Distinguish between the `Some` and `Nothing` variants.
+   */
   variant: Variant;
-  map<U>(this: Maybe<T>, mapFn: MapFn<T, U>): Maybe<U>;
-  mapOr<U>(this: Maybe<T>, orU: U, mapFn: MapFn<T, U>): U;
-  mapOrElse<U>(this: Maybe<T>, orElseFn: (...args: any[]) => U, mapFn: MapFn<T, U>): U;
+
+  map<U>(this: Maybe<T>, mapFn: (t: T) => U): Maybe<U>;
+  mapOr<U>(this: Maybe<T>, orU: U, mapFn: (t: T) => U): U;
+  mapOrElse<U>(this: Maybe<T>, orElseFn: (...args: any[]) => U, mapFn: (t: T) => U): U;
   or(this: Maybe<T>, mOr: Maybe<T>): Maybe<T>;
   orElse(this: Maybe<T>, orElseFn: (...args: any[]) => Maybe<T>): Maybe<T>;
   and<U>(this: Maybe<T>, mAnd: Maybe<U>): Maybe<U>;
@@ -32,6 +46,31 @@ export class Some<T> implements IMaybe<T> {
 
   variant = Variant.Some;
 
+  /**
+   * Directly create a `Maybe.Some` instance.
+   * 
+   * **Note:** While you *may* create the `Some` type via normal JavaScript
+   * class construction, it is not recommended for the functional style for
+   * which the library is intended. Instead, use [[of]] (for the general case)
+   * or [[some]] for this specific case.
+   * 
+   * ```ts
+   * // Avoid:
+   * const aString = new Maybe.Some('characters');
+   * 
+   * // Prefer:
+   * const aString = Maybe.some('characters);
+   * ```
+   * 
+   * @param value
+   * The value to wrap in a `Maybe.Some`.
+   * 
+   * `null` and `undefined` are allowed by the type signature so 
+   * that the constructor may `throw` on those rather than
+   * constructing a type like `Maybe<undefined>`.
+   * 
+   * @throws      If you pass `null` or `undefined`.
+   */
   constructor(value: T | null | undefined) {
     if (isCthulhu(value)) {
       throw new Error('Tried to construct `Some` with `null` or `undefined`');
@@ -40,15 +79,15 @@ export class Some<T> implements IMaybe<T> {
     this.__value = value;
   }
 
-  map<U>(this: Maybe<T>, mapFn: MapFn<T, U>): Maybe<U> {
+  map<U>(this: Maybe<T>, mapFn: (t: T) => U): Maybe<U> {
     return map(mapFn, this);
   }
 
-  mapOr<U>(this: Maybe<T>, orU: U, mapFn: MapFn<T, U>): U {
+  mapOr<U>(this: Maybe<T>, orU: U, mapFn: (t: T) => U): U {
     return mapOr(orU, mapFn, this);
   }
 
-  mapOrElse<U>(this: Maybe<T>, orElseFn: (...args: any[]) => U, mapFn: MapFn<T, U>): U {
+  mapOrElse<U>(this: Maybe<T>, orElseFn: (...args: any[]) => U, mapFn: (t: T) => U): U {
     return mapOrElse(orElseFn, mapFn, this);
   }
 
@@ -80,15 +119,15 @@ export class Some<T> implements IMaybe<T> {
 export class Nothing<T> implements IMaybe<T> {
   variant = Variant.Nothing;
 
-  map<U>(this: Maybe<T>, mapFn: MapFn<T, U>): Maybe<U> {
+  map<U>(this: Maybe<T>, mapFn: (t: T) => U): Maybe<U> {
     return map(mapFn, this);
   }
 
-  mapOr<U>(this: Maybe<T>, orU: U, mapFn: MapFn<T, U>): U {
+  mapOr<U>(this: Maybe<T>, orU: U, mapFn: (t: T) => U): U {
     return mapOr(orU, mapFn, this);
   }
 
-  mapOrElse<U>(this: Maybe<T>, orElseFn: (...args: any[]) => U, mapFn: MapFn<T, U>): U {
+  mapOrElse<U>(this: Maybe<T>, orElseFn: (...args: any[]) => U, mapFn: (t: T) => U): U {
     return mapOrElse(orElseFn, mapFn, this);
   }
 
@@ -124,32 +163,49 @@ export const some = <T>(value: T) => new Some<T>(value);
 export const nothing = <T>() => new Nothing<T>();
 
 /**
- * A value which may or may not be present.
- * 
- * If the value is present, it is `Some(value)`. If it's absent, it's `Nothing`.
- * This provides a type-safe container for dealing with the possibility that
- * there's nothing here – a container you can do many of the same things you
- * might with an array – so that you can avoid nasty `null` and `undefined`
- * checks throughout your codebase.
+ * A value which may (`Some<T>`) or may not (`Nothing`) be present.
  * 
  * The behavior of this type is checked by TypeScript at compile time, and bears
  * no runtime overhead other than the very small cost of the container object.
  */
 export type Maybe<T> = Some<T> | Nothing<T>;
+
+/**
+ * Create a `Maybe` from any value.
+ * 
+ * @typeparam T The type of the item contained in the `Maybe`.
+ * @param value The value to wrap in a `Maybe`. If it is `undefined` or `null`,
+ *              the result will be `Nothing`; otherwise it will be the type of
+ *              the value passed.
+ * 
+ * To specify that the result should be interpreted as a specific type, you may
+ * invoke of with a type parameter:
+ * 
+ * ```ts
+ * const foo = Maybe.of<string>(null);
+ * ```
+ * 
+ * This is usually only important in two cases:
+ * 
+ * 1.  If you are intentionally constructing a `Nothing` from a known `null` or
+ *     undefined value.
+ * 2.  If you are specifying that the type is more general than the value passed
+ *     (since TypeScript can define types as literals).
+ */
 export const of = <T>(value: T | undefined | null): Maybe<T> =>
   isCthulhu(value) ? nothing<T>() : some(value);
 
 export default Maybe;
 
-export const map = <T, U>(mapFn: MapFn<T, U>, mt: Maybe<T>): Maybe<U> =>
+export const map = <T, U>(mapFn: (t: T) => U, mt: Maybe<T>): Maybe<U> =>
   isSome(mt) ? some(mapFn(unwrap(mt))) : nothing<U>();
 
-export const mapOr = <T, U>(orU: U, mapFn: MapFn<T, U>, mt: Maybe<T>): U =>
+export const mapOr = <T, U>(orU: U, mapFn: (t: T) => U, mt: Maybe<T>): U =>
   isSome(mt) ? mapFn(unwrap(mt)) : orU;
 
 export const mapOrElse = <T, U>(
   orElseFn: (...args: any[]) => U,
-  mapFn: MapFn<T, U>,
+  mapFn: (t: T) => U,
   mt: Maybe<T>
 ): U => (isSome(mt) ? mapFn(unwrap(mt)) : orElseFn());
 
