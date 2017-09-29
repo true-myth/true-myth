b23b2dd3431314e142b2dab8bb2f062873808e1c
Chris Krycho <chris@chriskrycho.com>
2017-09-28 20:26:32 -0400

    Add utils.ts.

---
+++ src/utils.ts
@@ -0,0 +1,6 @@
+/**
+ * Check if the value here is an all-consuming monstrosity which will consume
+ * everything in its transdimensional rage. A.k.a. `null` or `undefined`.
+ */
+export const isVoid = (value: any): value is undefined | null =>
+  typeof value === 'undefined' || value === null;
