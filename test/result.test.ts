d8e52339d81d85d7acdb9d3ab696962b4fe0a6d1
Chris Krycho <chris@chriskrycho.com>
2017-09-29 08:47:00 -0400

    Require full coverage; include coverage by default; Result test stubs.

--- test/result.test.ts
+++ test/result.test.ts
@@ -1,4 +1,68 @@
-// import * as Result from 'result'
-test('this has a test', () => {
-  expect(true).toBe(true)
-})
+import { assertType } from './lib/assert';
+import * as Result from '../src/result';
+
+describe('`Result` static methods', () => {
+  test('`ok`', () => {
+    expect('to be implemented').toBe(false);
+  });
+
+  test('`err`', () => {
+    expect('to be implemented').toBe(false);
+  });
+
+  test('`map`', () => {
+    expect('to be implemented').toBe(false);
+  });
+
+  test('`mapOr`', () => {
+    expect('to be implemented').toBe(false);
+  });
+
+  test('`mapOrElse`', () => {
+    expect('to be implemented').toBe(false);
+  });
+
+  test('`mapErr`', () => {
+    expect('to be implemented').toBe(false);
+  });
+
+  test('`and`', () => {
+    expect('to be implemented').toBe(false);
+  });
+
+  test('`andThen`', () => {
+    expect('to be implemented').toBe(false);
+  });
+
+  test('`or`', () => {
+    expect('to be implemented').toBe(false);
+  });
+
+  test('`orElse`', () => {
+    expect('to be implemented').toBe(false);
+  });
+
+  test('`unwrap`', () => {
+    expect('to be implemented').toBe(false);
+  });
+
+  test('`unwrapErr`', () => {
+    expect('to be implemented').toBe(false);
+  });
+
+  test('`unwrapOrElse`', () => {
+    expect('to be implemented').toBe(false);
+  });
+
+  test('`toMaybe`', () => {
+    expect('to be implemented').toBe(false);
+  });
+});
+
+test('`Result.Ok` class', () => {
+  expect('to be implemented').toBe(false);
+});
+
+test('`Result.Err` class', () => {
+  expect('to be implemented').toBe(false);
+});
