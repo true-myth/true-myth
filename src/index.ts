65253a73c9d55be87d76df4758b9cf109fef4d7e
Chris Krycho <chris@chriskrycho.com>
2017-10-22 19:03:49 -0600

    Add trivial docs for index.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/**
+  This is just here to re-export [`Maybe`](./_maybe_.html) and
+  [`Result`](./_result_.html). It doesn't do anything else.
+ */
+
+/** (keep typedoc from getting confused by the imports) */
 import * as Maybe from './maybe';
 import * as Result from './result';
 
