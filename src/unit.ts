e3419f2cabe0ab3cb91a308a346c7095264cf1d2
Chris Krycho <hello@chriskrycho.com>
2021-11-27 13:38:40 -0700

    Update docs for Unit

--- src/unit.ts
+++ src/unit.ts
@@ -1,11 +1,17 @@
+const TagSymbol = Symbol('BrandTag');
+
 class Brand<Tag extends string> {
-  constructor(protected readonly __tag: Tag) {}
+  protected readonly [TagSymbol]: Tag;
+
+  constructor(tag: Tag) {
+    this[TagSymbol] = tag;
+  }
 }
 
 /**
   The `Unit` type exists for the cases where you want a type-safe equivalent of
   `undefined` or `null`. It's a concrete instance, which won't blow up on you,
-  and you can safely use it with e.g. [`Result`](../modules/_result_.html)
+  and you can safely use it with e.g. {@linkcode result.Result Result}
   without being concerned that you'll accidentally introduce `null` or
   `undefined` back into your application.
  */
