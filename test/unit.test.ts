c08c1c32782d1dcbeb471cb16d8cc97d34738f29
Chris Krycho <hello@chriskrycho.com>
2021-11-26 19:14:05 -0700

    Update to latest expect-type
    Fix type test check for Unit

--- test/unit.test.ts
+++ test/unit.test.ts
@@ -2,7 +2,7 @@ import { expectTypeOf } from 'expect-type';
 import Unit from 'true-myth/unit';
 
 test('the unit type', () => {
-  expectTypeOf<Unit>().toEqualTypeOf();
+  expectTypeOf<Unit>().toEqualTypeOf<{}>();
   expectTypeOf({}).not.toMatchTypeOf(Unit);
 
   expect(Unit).not.toEqual({});
