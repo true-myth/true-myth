5bacc45b60301f49232e9cb21f77495eb4885489
Chris Krycho <hello@chriskrycho.com>
2022-04-22 06:32:18 -0600

    Incorporate toString handling from v5
    (cherry picked from commit 25adb1aa0b4c02c7bf980c9db3fac7ab416de7b7)
    
    ---
    
    Original message:
    
    Add support for TypeScript 4.7
    
    TS 4.7 catches a class of error which 4.6 and earlier did not, courtesy
    of microsoft/TypeScript#43183: if the type passed does not implement
    `toString()`, it notices, because it no longer defaults to falling back
    to `{}`.
    
    To resolve this, *loosen* the constraints on what is acceptable as
    input to `toString`, by making the implementation itself more robust.
    Doing so also fixes a bug with the output for `toString` when working
    with strings: previously if you had `Maybe("a string")`, the `toString`
    output would be `'Maybe(a string)'`; it is now `'Maybe("a string")'`.
    
    Fixes #330

--- src/-private/utils.ts
+++ src/-private/utils.ts
@@ -16,3 +16,20 @@ export const isVoid = (value: unknown): value is undefined | null =>
 export function curry1<T, U>(op: (t: T) => U, item?: T) {
   return item !== undefined ? op(item) : op;
 }
+
+/**
+ * Check whether a given key is in an object
+ * @internal
+ */
+function has<T, K extends PropertyKey>(value: T, key: K): value is T & { [Key in K]: unknown } {
+  return typeof value === 'object' && value !== null && key in value;
+}
+
+export function safeToString(value: unknown): string {
+  if (has(value, 'toString') && typeof value['toString'] === 'function') {
+    const fnResult = value.toString();
+    return typeof fnResult === 'string' ? fnResult : JSON.stringify(value);
+  } else {
+    return JSON.stringify(value);
+  }
+}
