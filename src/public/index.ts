de23adf09228eebd67e020467655f742ed578423
Chris Krycho <hello@chriskrycho.com>
2022-07-07 21:08:22 -0600

    Introduce Toolbelt re-export
    Fixes #395

--- src/public/index.ts
+++ src/public/index.ts
@@ -1,9 +1,13 @@
 /**
-  This is just here to re-export [`Maybe`](./_maybe_.html),
-  [`Result`](./_result_.html), and [`Unit`](./_unit_.html), that is, to provide
-  a root-level entry amenable to use with node.
+  This is just here to re-export {@link Maybe}, {@link Result}, {@link Unit},
+  and {@link Toolbelt}  – that is, to provide a root-level entry amenable to use
+  with Node with TypeScript versions before 4.7 and its ESM support, or for
+  convenience.
+
+  @packageDocumentation
  */
 
 export { Maybe } from './maybe.js';
 export { Result } from './result.js';
 export { Unit } from './unit.js';
+export * as Toolbelt from './toolbelt.js';
