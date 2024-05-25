947adfd82c058a606b18e9a2e77f61b9e8f105ad
Chris Krycho <hello@chriskrycho.com>
2024-05-24 22:08:26 -0600

    Add failing tests for illegal `null`s in `Maybe`
    Forbid two cases which will throw at runtime after producing the type
    `Maybe<null>` (which is nonsensical!) at compile time:
    
    - Prevent `someMaybe.map((_) => null)`.
    - Prevent `Maybe.of((_) => null)`, which should generally only appear
      when someone is using the function with `.ap()`, itself quite rare,
      but important to avoid.

--- src/maybe.ts
+++ src/maybe.ts
@@ -492,8 +492,8 @@ export function mapOr<T, U>(
   return mapFn === undefined
     ? partialOp
     : maybe === undefined
-      ? partialOp(mapFn)
-      : partialOp(mapFn, maybe);
+    ? partialOp(mapFn)
+    : partialOp(mapFn, maybe);
 }
 
 /**
@@ -1487,7 +1487,7 @@ export function get<T, K extends keyof T>(
 export function wrapReturn<
   F extends AnyFunction,
   P extends Parameters<F>,
-  R extends NonNullable<ReturnType<F>>,
+  R extends NonNullable<ReturnType<F>>
 >(fn: F): (...args: P) => Maybe<R> {
   return (...args) => Maybe.of(fn(...args)) as Maybe<R>;
 }
