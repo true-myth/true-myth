d87497401ed7dee12e50bf1ad9f5364cf54f3d2e
Chris Krycho <hello@chriskrycho.com>
2025-04-01 22:45:06 -0600

    Task: use `IterableIterator` for `Strategy`
    This appears to *actually* work as my previous change wa s*intended* to
    work. See the new test case for an example of the change this catches!

--- src/task/delay.ts
+++ src/task/delay.ts
@@ -96,7 +96,7 @@
   );
   ```
  */
-export interface Strategy extends Iterable<number> {}
+export interface Strategy extends IterableIterator<number> {}
 
 /**
   Generate an infinite iterable of integers beginning with `base` and increasing
